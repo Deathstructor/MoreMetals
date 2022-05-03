33d934cbe5404d43703c7ae4e8707d0e5a1d1e21
Paul Chastain <paulchast1@gmail.com>
2022-05-03 17:49:01 +0200

    Added cobalt ingot, ore and block

---
+++ src/main/java/com/deathstructor/moremetals/setup/ModItems.java
@@ -0,0 +1,12 @@
+package com.deathstructor.moremetals.setup;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+
+public class ModItems {
+    public static final RegistryObject<Item> COBALT_INGOT = Registration.ITEMS.register("cobalt_ingot", () ->
+            new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+
+    static void register() {}
+}
--- src/main/java/com/deathstructor/moremetals/setup/Registration.java
+++ src/main/java/com/deathstructor/moremetals/setup/Registration.java
@@ -1,4 +1,23 @@
 package com.deathstructor.moremetals.setup;
 
+import com.deathstructor.moremetals.MoreMetals;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class Registration {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MoreMetals.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MoreMetals.MOD_ID);
+
+    public static void register() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+
+        ModBlocks.register();
+        ModItems.register();
+    }
 }
